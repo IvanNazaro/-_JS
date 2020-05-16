88743a3abc6d074e3c859f64aa0e49d1228876e8
IvanNazaro <65441189+IvanNazaro@users.noreply.github.com>
2020-05-16 15:22:50 +0300

    Добавил структуру
    Базовый шаблон  без CSS

---
+++ Practick.js
@@ -0,0 +1,12 @@
+let bth = document.querySelector('button').onclick = () => {
+    let che = document.querySelectorAll('option')
+    let inp = document.querySelector('.out').value
+    for (let i = 0; i < che.length; i++) {
+        if (che[i].selected) {
+            document.querySelector('.one').innerHTML = ' Цена: ' + Math.ceil(inp * che[i].value) + ' &#8381'
+
+        }
+
+    }
+
+}
